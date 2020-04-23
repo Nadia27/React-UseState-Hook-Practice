8b40c0641fc6da59b698ce4d200bd28ddac116e9
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-23 02:18:50 -0400

    Challenge 1
    Display latest time in h1 when button pressed using useState()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+//Challenge:
+
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
 
 function App() {
+  //1. Given that you can get the current time using:
+  let now = new Date().toLocaleTimeString();
+  console.log(now);
+
+  //Show the latest time in the <h1> when the Get Time button
+  //is pressed.
+  const [time, updateTime] = useState("TIME");
+
+  function getNewTime() {
+    updateTime(now);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getNewTime}>Get Time</button>
     </div>
   );
 }
