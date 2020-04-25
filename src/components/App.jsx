25a5182f30f3e90305fc461b464882142882159d
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-25 19:25:58 -0400

    useState()
    useState() to create a working clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,29 +6,25 @@ import React, { useState } from "react";
 //using the setInterval method.
 //Can you get the time in your <h1> to update every second?
 
-//e.g. uncomment the code below to see Hey printed every second.
-// function sayHi() {
-//   console.log("Hey");
-// }
-// setInterval(sayHi, 1000);
-
 function App() {
+  setInterval(displayTime, 1000);
+
   //1. Given that you can get the current time using:
   let now = new Date().toLocaleTimeString();
-  console.log(now);
 
   //Show the latest time in the <h1> when the Get Time button
   //is pressed.
-  const [time, updateTime] = useState("TIME");
+  const [time, setTime] = useState(now);
 
-  function getNewTime() {
-    updateTime(now);
+  function displayTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
   }
 
   return (
     <div className="container">
       <h1>{time}</h1>
-      <button onClick={getNewTime}>Get Time</button>
+      {/* <button onClick={displayTime}>Get Time</button> */}
     </div>
   );
 }
